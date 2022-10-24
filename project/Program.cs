bcb51b3dc991c72391cdf019810e6a9cd5c6e255
Svvida <ksvvida@gmail.com>
2022-10-24 21:43:22 +0200

    Add Checking if data is in hexadecimal format

--- project/Program.cs
+++ project/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Simulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Podaj wartość AH:");
+
+            string AH = Console.ReadLine();
+            bool hexa = IsHex(AH);
+            System.Console.WriteLine(AH);
+            System.Console.WriteLine(hexa);
+
+        }
+        static bool IsHex(IEnumerable<char> chars)
+        {
+            bool isHex;
+            foreach (var c in chars)
+            {
+                isHex = ((c >= '0' && c <= '9') ||
+                          (c >= 'a' && c <= 'f') ||
+                           (c >= 'A' && c <= 'F'));
+
+                if (!isHex)
+                    return false;
+            }
+            return true;
+        }
+    }
+
+}
